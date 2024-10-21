506ff92c547a40fc5685cd3bc853c9311c45c3c1
Ahmed Badawi <ahmedbadawi.1411@gmail.com>
2024-10-21 16:17:38 +0300

    home page logic

--- src/app/components/events-home/events-home.component.ts
+++ src/app/components/events-home/events-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-events-home',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./events-home.component.css']
 })
 export class EventsHomeComponent {
+  events:any;
+  
+  constructor(
+    private dataService: DataService
+  ) {
 
+      this.dataService.getData().subscribe(
+        (response: any[]) => {
+          this.events = response.slice(5, 8).map((item: any) => ({
+            title: item.name,
+            description: `${item.p2}.`,
+            location: item.location,
+            companyName: item.providerName,
+            website: item.providerwebsite,
+            providerImg: item.img1,
+            img1: item.img1,
+            img2: item.img2,
+            img3: item.img3,
+            eventId: item.id - 1,
+          }));
+        });
+  }
 }
--- src/app/components/tours-home/tours-home.component.ts
+++ src/app/components/tours-home/tours-home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-tours-home',
   templateUrl: './tours-home.component.html',
   styleUrls: ['./tours-home.component.css']
 })
-export class ToursHomeComponent {
-
+export class ToursHomeComponent{
+  tours:any;
+  constructor(
+    private dataService: DataService
+  ) {
+    this.dataService.getData().subscribe(
+      (response: any[]) => {
+        this.tours = response.slice(2, 5).map((item: any) => ({
+          title: item.name,
+          description: `${item.p2}.`,
+          location: item.location,
+          companyName: item.providerName,
+          website: item.providerwebsite,
+          providerImg: item.img1,
+          img1: item.img1,
+          img2: item.img2,
+          img3: item.img3,
+          eventId: item.id - 1,
+        }));
+      });
+  }
 }
